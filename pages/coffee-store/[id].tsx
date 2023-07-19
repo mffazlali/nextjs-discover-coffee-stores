224c2d345077d86516b8fe6b19cdd24e957ae357
mffazlali <mf.fazlali@gmail.com>
2023-07-19 16:27:27 +0330

    add font avesome

--- pages/coffee-store/[id].tsx
+++ pages/coffee-store/[id].tsx
@@ -2,14 +2,16 @@ import { useRouter } from 'next/router'
 import Head2 from 'next/head'
 import coffeeStoresData from '../../store/coffee-stores.json'
 import { GetStaticPaths, GetStaticProps } from 'next'
+import Link from 'next/link'
+import Script from 'next/script'
 
 export const getStaticPaths: GetStaticPaths<{ id: string }> = async () => {
   const paths = coffeeStoresData.map((coffeeStore) => {
-    return { params: { id: String(coffeeStore.id) } }
+    return { params: { id: coffeeStore.id.toString() } }
   })
   return {
     paths,
-    fallback: false,
+    fallback: true,
   }
 }
 
@@ -26,17 +28,37 @@ export const getStaticProps: GetStaticProps = async (staticProps) => {
 
 const CoffeeStore = (props: any) => {
   const router = useRouter()
-  const id = router.query.id
+  // const id = router.query.id
   console.log('router', router)
   console.log('props', props)
+
+  if (router.isFallback) {
+    return (
+      <div className="flex justify-center items-center h-screen flex-col">
+        <span className="block w-32 h-32 rounded-full border-[8px] border-black border-t-black/10 border-r-black/10 animate-spin"></span>
+        <span className="text-xl font-medium capitalize">loading</span>
+      </div>
+    )
+  }
+
+  const { id, name, neighbourhood, address } = props.coffeeStore
+
   return (
     <div>
       <Head2>
-        <title>{id}</title>
+        <title>{props.coffeeStore.name}</title>
+        <Script
+          src="/public/fonts/fontawesome.js"
+          crossOrigin="anonymous"
+        ></Script>
       </Head2>
-      <div>coffee store {id}</div>
-      <p>{props.coffeeStore.address}</p>
-      <p>{props.coffeeStore.name}</p>
+      <Link href={'/'}>
+        <i className="fa fa-arrow-left"></i>
+      </Link>
+      <i className="fa fa-arrow-left">a</i>
+      <p>{address}</p>
+      <p>{name}</p>
+      <p>{neighbourhood}</p>
     </div>
   )
 }
